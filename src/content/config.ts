5d91f19df2a0cecdbc7092b00907d8d6880578f9
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2026-04-09 02:49:30 -0300

    add tags and fix year index

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,8 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
-    draft: z.boolean().optional()
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
